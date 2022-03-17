e9383f99056207e89a20acdc21933b329410b2d3
JeremiJD <jeremidjeremi@gmail.com>
2022-03-17 13:11:38 +0100

    Main Activity done

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.RadioButton
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val lista =  findViewById<TextView>(R.id.textView2)
+
+        findViewById<RadioButton>(R.id.chicken).setOnClickListener {
+            if (findViewById<RadioButton>(R.id.chicken).isChecked) {
+                lista.append("Kurczak\n")
+            }
+        }
+        findViewById<RadioButton>(R.id.Noodle).setOnClickListener {
+            if (findViewById<RadioButton>(R.id.Noodle).isChecked) {
+                lista.append("Noodle\n")
+            }
+        }
+        findViewById<RadioButton>(R.id.Sorbet).setOnClickListener {
+            if (findViewById<RadioButton>(R.id.Sorbet).isChecked) {
+                lista.append("Sorbet Malinowy\n")
+            }
+        }
+        }
     }
-}
